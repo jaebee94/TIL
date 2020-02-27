f25ff8150d78c2d6df9b24f9c22b77d6613570a5
jaebee94 <woqls8449@gmail.com>
2020-02-27 17:19:24 +0900

    200227

--- BAEKJOON/단계별로 풀어보기/13브루트포스/1018_체스판다시칠하기.py
+++ BAEKJOON/단계별로 풀어보기/13브루트포스/1018_체스판다시칠하기.py
@@ -27,3 +27,39 @@ for x in range(N-7):
             minn = cnt
             print(x, y, minn)
 print(minn)
+
+#####경은#####
+# N, M = map(int, input().split())
+# matrix = [list(map(str, input())) for _ in range(N)]
+# for i in range(N):
+#     for j in range(M):
+#         if matrix[i][j] == 'B':
+#             matrix[i][j] = 0
+#         else:
+#             matrix[i][j] = 1
+# repaint1 = [[0]*(M-7) for _ in range(N-7)]
+# repaint2 = [[0]*(M-7) for _ in range(N-7)]
+# for i in range(N-7):
+#     for j in range(M-7):
+#         for k in range(i, i+8):
+#             for l in range(j, j+8):
+#                 if ((k-i + l-j) % 2) and matrix[i][j] == matrix[k][l]:
+#                     repaint1[i][j] += 1
+#                 if ((k-i + l-j) % 2 == 0) and matrix[i][j] != matrix[k][l]:
+#                     repaint1[i][j] += 1
+# for i in range(N-7):
+#     for j in range(M-7):
+#         for k in range(i, i+8):
+#             for l in range(j, j+8):
+#                 if ((k-i + l-j) % 2) and not(matrix[i][j]) == matrix[k][l]:
+#                     repaint2[i][j] += 1
+#                 if ((k-i + l-j) % 2 == 0) and not(matrix[i][j]) != matrix[k][l]:
+#                     repaint2[i][j] += 1
+# minn = 64
+# for i in range(N-7):
+#     for j in range(M-7):
+#         if repaint1[i][j] < minn:
+#             minn = repaint1[i][j]
+#         if repaint2[i][j] < minn:
+#             minn = repaint2[i][j]
+# print(minn)
--- BAEKJOON/단계별로 풀어보기/13브루트포스/2231_분해합.py
+++ BAEKJOON/단계별로 풀어보기/13브루트포스/2231_분해합.py
@@ -8,3 +8,19 @@ for n in range(N):
         break
     result = 0
 print(result)
+
+#####경은#####
+import sys
+N = int(sys.stdin.readline())
+n = N//10
+while n < N:
+    gen = list(map(int, str(n)))
+    total = sum(gen) + n
+    if total == N:
+        result = ''.join(map(str, gen))
+        break
+    n += 1
+    if n== N:
+        result = 0
+        break
+print(result)
