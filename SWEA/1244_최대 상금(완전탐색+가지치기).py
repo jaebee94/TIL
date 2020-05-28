ee8bfcd783c3b3ff8dad0af2e09d32a64c34a48e
woqls8449 <woqls8449@gmail.com>
2020-05-28 23:34:28 +0900

    -

---
+++ SWEA/1244_최대 상금(완전탐색+가지치기).py
@@ -0,0 +1,10 @@
+for tc in range(1, int(input())+1):
+    num, n = input().split()
+    n = int(n)
+
+    s = [num]
+    while s:
+        num = s.pop()
+        used = [num]
+        for 
+    # print(arr, n)
